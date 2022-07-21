20764af54d1e28c3febad26b9ffae81c85bb8ce0
ffanecaa <ffanecaa@gmail.com>
2022-07-21 13:22:06 +0200

    m

--- js/web/prompt.js
+++ js/web/prompt.js
@@ -1,1 +1,11 @@
 //salida con alert entrada datos prompt//
+/*const number = document.getElementById('ele');
+let edad;
+edad = prompt('Introduce tu edad');
+
+ele.textContent = `la edad introducida es ${edad}`;*/
+
+
+edad=prompt('introduce tu edad');
+
+document.getElementById('ele').innerHTML=` la edad introducida es <h1> ${edad}</h1>`;
